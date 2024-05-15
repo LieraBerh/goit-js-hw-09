67b34cf62d073f9c842f9ebaa9872dfae8e64207
LieraBerh <lierrastern@gmail.com>
2024-05-15 17:16:33 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,3 +6,35 @@ const formData = {
 const formEl = document.querySelector('.feedback-form');
 
 formEl.addEventListener('input', handleFormInput);
+
+function handleFormInput(event) {
+  formData.email = formEl.elements.email.value;
+  formData.message = formEl.elements.message.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+const storedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (storedFormData) {
+  formData.email = storedFormData.email;
+  formData.message = storedFormData.message;
+  formEl.elements.email.value = formData.email;
+  formEl.elements.message.value = formData.message;
+}
+
+formEl.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  formEl.reset();
+}
